85dc732b2adb206e67657567985b2b726e171e39
Vivek Vaish <vaishvivek634@gmail.com>
2023-11-06 14:47:33 +0530

    navbar complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
